34a7f1a7a95a57dc86e09f1ced2322586e139f96
akrocky <abul64604@gmail.com>
2022-05-29 11:12:14 +0200

    first commit

---
+++ src/pages/Register.jsx
@@ -0,0 +1,99 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import Logo from "../assets/ak_log.png";
+
+function Register() {
+  const onhandleSubmit = (event) => {
+    event.preventDefault();
+    alert("form");
+  };
+  const handleChange = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <>
+      <FormContainer>
+        <form onSubmit={(event) => onhandleSubmit(event)}>
+          <div className="brand">
+            <img src={Logo} alt="" />
+            <h1>AK Demo Chat Application </h1>
+          </div>
+          <input
+            type="text"
+            placeholder="username"
+            name="username"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            type="email"
+            placeholder="email"
+            name="email"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            name="password"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            type="password"
+            placeholder="confirm password"
+            name="confirm password"
+            onChange={(e) => handleChange(e)}
+          />
+
+          <button type="submit">Create User</button>
+          <span>
+            Already have an account? <Link to="/login">Login</Link>
+          </span>
+        </form>
+      </FormContainer>
+    </>
+  );
+}
+const FormContainer = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+  align-items: center;
+  background-color: #131324;
+  .brand {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    justify-content: center;
+    img {
+      height: 5rem;
+    }
+    h1 {
+      color: white;
+      text-transform: uppercase;
+    }
+  }
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    background-color: #00000076;
+    border-radious: 2rem;
+    padding: 3rem 5rem;
+    input {
+      background-color: transparent;
+      padding: 1rem;
+      border: 0.1rem solid #4e0eff;
+      border-radius: 0.4rem;
+      color: white;
+      width: 100%;
+      font-size: 1rem;
+      &:focus {
+        border: 0.1rem solid #997af0;
+        outline: none;
+      }
+    }
+  }
+`;
+export default Register;
